05b569c3a6ecbd0eea8521dfff18aa61e0852458
Vincent Van Buskirk <vvanbuskirk805@gmail.com>
2017-04-03 11:35:34 -0700

    Completed all tests

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,20 @@
-var Bee = function() {
+var Grub = function() {
+  this.age = 0;
+  this.color = "pink";
+  this.food = "jelly";
 };
 
+Grub.prototype.eat = function() {
+
+}
+
+var Bee = function() {
+  Grub.call(this);
+  this.age = 5;
+  this.color = "yellow";
+  this.job = "keep on growing";
+
+}
+
+Bee.prototype = Object.create(Grub.prototype)
+Bee.prototype.constructor = Bee
--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,12 @@
 var Grub = function() {
+  this.age = 0;
+  this.color = "pink";
+  this.food = "jelly";
+  this.eat = function() {
+
+  }
 };
 
+Grub.prototype.eat = function() {
+
+}
